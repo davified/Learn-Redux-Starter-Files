9e6dc73a90d2aa5030e513f5d05459508c34c5be
davified <davified@gmail.com>
2017-03-08 16:07:50 +0800

    Add a subreducer function in comments reducer for adding/removing comments

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,12 +1,32 @@
 // a reducer takes in: (i) the action (info about what happened) and (ii) a copy of the current state, and **returns an updated store**
 
-function comments (state = [], action) {
-  switch(action.type) {
+function postComments (state = [], action) {
+  switch (action.type) {
     case 'ADD_COMMENT':
-      // return updated state
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT':
+      console.log('removing comment')
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ]
     default:
       return state
   }
+}
+
+// reducer composition
+function comments (state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state
 }
 
 export default comments
